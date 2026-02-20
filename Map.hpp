19d524ff3737649cd11732b4f7694c31695b08e7
Thomas-August <tommy.august@btinternet.com>
2026-02-20 18:05:03 +0000

    Renamed the class to Map as it stores the map and does raycasing with the map. Created the basics of the Map.hpp file

---
+++ Map.hpp
@@ -0,0 +1,15 @@
+// Ray.hpp
+// Define a class for raycasting
+//
+#include <vector>
+
+class Map { 
+    public: 
+        Map(const std::vector<std::vector<int>>& map);
+        float raycast(std::vector<float> position, std::vector<float> direction);
+        std::vector<std::vector<int>>& getMap();
+
+    private:
+        const std::vector<std::vector<int>>& map_;
+
+}
