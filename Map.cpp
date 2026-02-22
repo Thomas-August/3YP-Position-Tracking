f3b3c51e12dc2884bf107995a1e040484a7d6bc0
Thomas-August <tommy.august@btinternet.com>
2026-02-22 00:24:43 +0000

    Added the max height of the map and the max ray distance as variables of the Map class and started implementation of the raycast function

--- Map.cpp
+++ Map.cpp
@@ -4,7 +4,7 @@
 #include "Map.hpp"
 #include <Eigen/Dense>
 
-Map::Map(const Eigen::MatrixXi& map, float gridSize) : map_(map), gridSize_(gridSize) {}
+Map::Map(const Eigen::MatrixXi& map, float mapGridSize, float mapHeight, float maxRayDistance) : map_(map), mapGridSize_(mapGridSize), mapHeight_(mapHeight), maxRayDistance_(maxRayDistance) {}
     // A constructor to initialize the map and grid size.   
 
 Eigen::MatrixXi& Map::getMap() {
@@ -17,5 +17,13 @@ float Map::raycast(Eigen::Vector3f position, Eigen::Vector3f direction) {
     // This fucntion projects a horizontal ray from the given position in the direction of the given direction projected into the xy plane,
     // checks for collisions with the map walls, then checks if the ray has exceeded the vertical bounds at that point, and returns the
     // distance to the collision point with the wall or the floor/ceiling or the max ray distance, whichever is closer.
+
+    // Ensure that the direction vector is a unit vector
+    direction.normalize();
+
+    // Get the xy component of the direction vector without normalizing so the distance can bet applied to the z component later to 
+    // find height at the collision point and check for floor and ceiling collisions.
+    Eigen::Vector2f directionXY = direction.head<2>();
+
     return 0.0f;
 }
--- Map.hpp
+++ Map.hpp
@@ -5,8 +5,8 @@
 
 class Map { 
     public: 
-        Map(const Eigen::MatrixXi& map, float gridSize);
-            // A constructor to initialize the map and grid size.   
+        Map(const Eigen::MatrixXi& map, float mapGridSize, float mapHeight, float maxRayDistance);
+            // A constructor to initialize the max ray distance, map and its grid size and height.  
         Eigen::MatrixXi& getMap();
             // A function to return a reference to the map matrix.
         float raycast(Eigen::Vector3f position, Eigen::Vector3f direction);
@@ -20,7 +20,11 @@ class Map {
             // A reference to the map matrix, where each cell contains an integer representing the type of cell 
             // (0 for no walls, 1 for top wall, 2 for left wall, and 3 for both walls)
             // (right and bottom walls are not represented in the map as they will be checked for using the adjacent cells).
-        float gridSize_;
+        float mapGridSize_;
             // The size of each grid cell in the map, used to calculate distances in the raycasting algorithm.
+        float mapHeight_;
+            // The height of the map, used to check for floor and ceiling collisions in the raycasting algorithm.
+        float maxRayDistance_;
+            // The maximum distance the ray can measure
 
 };
